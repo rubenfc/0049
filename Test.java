784752362bf2eb84d5460fb334c6de2610077fc9
unknown <rubenfc2013@gmail.com>
2015-01-12 09:50:28 +0100

    creado el metodo multiplesOffFive

---
+++ Test.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Test here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Test
+{
+    public void multiplesOggFive()
+    {
+        int multiples = 15;
+        while (multiples < 95)
+        { 
+            System.out.println(multiples);
+            multiples = multiples + 5;
+        }
+    }
+}
