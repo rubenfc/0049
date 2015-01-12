554791bb9fb01218584068ef95d1c9aad5c63cd7
unknown <rubenfc2013@gmail.com>
2015-01-12 10:04:15 +0100

    creado el metodo sumValues que suma los 10 primeros numeros

--- Test.java
+++ Test.java
@@ -7,13 +7,28 @@
  */
 public class Test
 {
-    public void multiplesOggFive()
+    public void multiplesOffFive()
     {
         int multiples = 15;
         while (multiples < 95)
         { 
             System.out.println(multiples);
             multiples = multiples + 5;
+        }
+    }
+    
+    public void sumValues()
+    {
+        int index = 0;
+        int sumValue = 0;
+        int incremento = 1;
+        
+        while(index < 10 )
+        {
+            sumValue = sumValue + incremento;
+            System.out.println(sumValue);
+            index = index + 1;
+            incremento = incremento + 1;
         }
     }
 }
